9bf245c49285993c0ed24486d2264b4158b2c6c9
Daniel Weibel <danielmweibel@gmail.com>
2020-11-12 11:27:55 +0100

    Transform comments into docstrings and clean code

--- src/pm.py
+++ src/pm.py
@@ -1,144 +1,11 @@
 #!/usr/bin/env python3
 
-# Example parameters:
-# Password: 12345678
-# Salt: 4v8jf5fB1X1U/ougn9hDBg==
-# Hashed password: zIUnc.tpMRP4rhV27Iv3jG/SngcRHNK
-# Private key (encrypted):
-#    -----BEGIN PGP PRIVATE KEY BLOCK-----
-#    Version: ProtonMail
-#    
-#    xcMGBF+pAFYBCACr66UDk5JEGC+jE0UZIdKqpJJtVQgG16b0edcno0+bNY7z
-#    YctH7RlERN6IWJ5s5F8h7m+QoVgh8Rh6pHIBkdV2qRVwy4LUDijhpoqXnPxu
-#    OC5mjfc6MeMcADP6cZRrdLMT1FP35g6FfNtsMriejUxwQDm2xqj4pg0fSNbW
-#    FK4nz3CahRY7Jnj8DDz9eHnNFlpy6fx2Wa2gcqD0/cLXOgI18MUjqd4h5q4H
-#    f0qBdnkFS+FoV8sepUq3i+xuXwrVCDcVmGpAOY3MGoYrJpnJW8xWMGSLA2hr
-#    ZaimL+7/UwfvYgrYqVmxBiR0NKsfNQ0l0HroKN8UqDaGKhImbLU+tUHVABEB
-#    AAH+CQMIXwWzJ4NGTrVgomFjWqJioud7V6hTScYVFpZqEXNCXbNogAAAyrAH
-#    X+tIN7pEjVZXsffPgokCg7dqhuaoEjFAh21VLbd69p1iP+f17jSOg5vMq7FB
-#    f9iR/0xceQtm83juTcjV5h5r1b5khNBUX/wtMSEBd4laELf7GHuTtFTZqcLI
-#    TL+l7Hm5ve13Q+q0MN5G/bU8b4ojGHb9sI3F4tuthFWWXmqGglmLPygRO91v
-#    2poa+FVI66Z4pIpqN30V4cCzsYNK/1B9o67qyUU68aqtIuIhnaqijG0nFESA
-#    XzoMJsXyBirfcAnB5fi8Jo25XAbpicku9sad+w1KT+738cfx4Fvx10vTOtwS
-#    6SFXtu7w581R6hDlYa38vKSwx/y1SmWzBooBSaGMEj23hoCpCecz2FreL5kb
-#    wgg3U8V98DzaV6YpWjaR1sJoDevi6Bj44tOux1qEooKJyQ/VuHz5mT0tde7I
-#    f/UNZb+7ltoUUo6W4K9Go3ZAJr1JjIG7Cj+CPQmRTNHA/kLaMBLOZ+MhB9DK
-#    8lHpUeTlWNhnZ+6+2cJWFrGXztRwoK8TXOgAluhipCH6uLpfAcjmf8mpGrWo
-#    Qf33kkSgADy4EtXqQ3R4wf2XaxRenNFivQXaPXhMBlim8+r/GzYOIXXMyZJ8
-#    DDA31FltAHsVQsmaOEEgCHGMOJ/mLQdAVHBwcdOBWx3gxyuvl0JXtnR8ujY1
-#    Y3BaztrHiIUDmFzl4+fI/OBwWIg4ShOJb+mNwoW6mv240ltK+W7Fd9bCxspP
-#    ZivXCLPDH0bEAOvkUkoD8Keya+gdGYwyPgc9hEvgO7OdbdEXIURzP4TO2NLF
-#    1I2e2o0zkD4RHpKlASxR8mUjfbHxkoW4/jm1cYLiLLilFuK0FRnoCVHor/de
-#    eFzkKWKZjqF39E974k7bmLrrVJ9qQ4w6zTV1aHpnc2x2dHBoQHByb3Rvbm1h
-#    aWwuY29tIDx1aHpnc2x2dHBoQHByb3Rvbm1haWwuY29tPsLAjQQQAQgAIAUC
-#    X6kAVgYLCQcIAwIEFQgKAgQWAgEAAhkBAhsDAh4BACEJEMiY6iN4o+tmFiEE
-#    LSQyWbaWeeULxzIzyJjqI3ij62ZPkQf/acGp44US33eZdYAx620GgYbvnbJw
-#    PCfIqI1qIzuPs+dAkoqyAs/t6LjN45SkMKFoioZPW4D1EuVBNVYvj+gaMWfT
-#    3V9tON+xbvwu8acxqePkQWLikVkMvybAhoH6O9N4SqLG+u17lAjqyXN/qnqG
-#    xDkyGBDHnXx4PnaRATMZDwVrZd7rq5tT2Be2aOCwNzEE0pgoefBdr4+DF/Qk
-#    plhPBJBUiUzX04pAImRMioQX7LpejhLR6ao1FceXd1UyfMyj+OeAIeRxzdh2
-#    Nl+qhs5rRjB453LzMFrq5azEziPsqGVxqMCvNAmh4+0v8dW/DJu044Wh0erO
-#    QmW8mn9jKxjQXcfDBgRfqQBWAQgAsIQaXg9X2zqAOb9GcT7MkZDjZkGSnVEv
-#    qJUZ0JV0N4agCx4iktIePrXXDd9/k9Jfjgl33PAEV3RAA+GUY4Jq1mag5TEO
-#    wFIc2AES1TDjLJ2nZUIN6HnlzUZ5zQCHAgea/RUAM+QiL1ggylQjzIm/ZBAz
-#    j2vOqZgwNDSKlqkW4sIxkf0YxR/GrfRwMEqL37n+486C8XbzaLGcEjDIIWDQ
-#    VxvWysAhYSCktTPM+ti7F+uuuTdVuSDqxb4WZ3+0+ZL5xsjig9V0oakY03xY
-#    sSUOYDN8+0afXpz6ZVtKzH1vZd3gNFS3wrqLq0ft/IaMdVBo2B6xR5nbZZKq
-#    ucQxfdhlvwARAQAB/gkDCEudPAxoXM9lYPwzs4LAiNuYDP8Ta3GzqgLu/7QZ
-#    A/2RzHBMiSRbIHCOZJpR6TzHLj4sLWHZ+Yk0PfqvjU4fvhctRZe6jjDUH3Wl
-#    Xhu3mfGWoxeY2afEiKFJ8hp/w5c0lIHEA6WJPaSqfngbES/cjJv39On4HCmd
-#    jFmBKx9rSNOjATPciX1oaRI3pP1TgZXtIGsjequIi1ZPs5fM/NnHWap1tm5A
-#    Jm6MHGZSYoThVnl2PZ9qfEz6UM2XIn3LjnI07hHe+h+Xq5Yb5EDTKVcmnqws
-#    YCATkAGC9ZvNsZBr05fBt28MKMoTwkAQCgqvTpmPAfSyfD5a2/Su1gmRYONY
-#    p8sjatOFX2cOiaMd+69UQJ9L6EF4MhgYkqutnQD1LngYA88PqIuSYe6tytlO
-#    6Hye+A/w7GYTjW0/0Fiv+JYE5eXJnLe/AJbQXRfGUQfMEOVMD/w77hULv2H4
-#    bcA+kRluidBfskRJySddxaLSeUwmMSPP0WnOUbYihUCkuh1QhdGs8BGgqE+b
-#    NFLoWpRGvpLrigO5QkyFfwzQgP/oVB3B5pzpeDcTTif+S08jmOjdPD6/5ytS
-#    duqB9VMm/sOk+/ceZ7qRK/PmpBjU+duaDLWGc80W+AVoMvcJzQG3oQBAej4C
-#    asRzubiKOC5e0YVyq/6fuz+p/Luzw16yBwX43d0X/F1GFPwequVYouGHax4D
-#    V1IcBeglfo7iY4W+rERQXiNyRfQMxbrdASo8DxeVvev7+/HK+uPoByJmXKEf
-#    THgfpo/zdKaBiIBvU3xLoLxZeLxkzIaYJgRlWT40rgM94D2l+31AwLlCtnz9
-#    nDMboK5CtXzVEzjallb7H2XCXoS0pWX2/BqWohK5UWc7FuJXQUNYv6efVtBT
-#    6EGBANDo/e6dX+lniQlS28NaUdc4i9evzUSYH241+/g5n8LAdgQYAQgACQUC
-#    X6kAVgIbDAAhCRDImOojeKPrZhYhBC0kMlm2lnnlC8cyM8iY6iN4o+tmQV8H
-#    /3h1GZOJQFPWYaP2ctE4xXxbDSGOf3u1lx8NukypE4lGzSBjEQTXcPRPovaM
-#    CSF5IGDAStI6iMjQH8W3k7nYPaJXdgJ6n8qw+c97A4YUDAXGcEqZgrEpRtmz
-#    o2N1xCt3n/AFVCdSGU6Y2TarT3MfmZD6rVZTmyUYSbT8hJiHU3TekHlpoVw8
-#    d54H4H/koaQQC9UA/Eg9SbxjCF2RyyrmPfGsj3pnGQhOO0AGRZuqSQXSaoD0
-#    EvyLGUFUFRgx7lP1pG2V2C8139dFjGA7bTGt1wMY8kwVvcBZLQzfWnxMAjSB
-#    /I1bhPEIC0rTXluApGBOaYF7mcTrXeFeOxR7BpHi5rA=
-#    =tu9M
-#    -----END PGP PRIVATE KEY BLOCK-----
-# Private key (decrypted):
-#    -----BEGIN PGP PRIVATE KEY BLOCK-----
-#    Version: OpenPGP.js v4.10.8
-#    Comment: https://openpgpjs.org
-#    
-#    xcLYBF+pAFYBCACr66UDk5JEGC+jE0UZIdKqpJJtVQgG16b0edcno0+bNY7z
-#    YctH7RlERN6IWJ5s5F8h7m+QoVgh8Rh6pHIBkdV2qRVwy4LUDijhpoqXnPxu
-#    OC5mjfc6MeMcADP6cZRrdLMT1FP35g6FfNtsMriejUxwQDm2xqj4pg0fSNbW
-#    FK4nz3CahRY7Jnj8DDz9eHnNFlpy6fx2Wa2gcqD0/cLXOgI18MUjqd4h5q4H
-#    f0qBdnkFS+FoV8sepUq3i+xuXwrVCDcVmGpAOY3MGoYrJpnJW8xWMGSLA2hr
-#    ZaimL+7/UwfvYgrYqVmxBiR0NKsfNQ0l0HroKN8UqDaGKhImbLU+tUHVABEB
-#    AAEAB/wIqL6KZX3V6LBI4tuq26Qtp0rfGgvks6rLgFyIzlC1j6xyz++Uy0Qn
-#    xwk8rav9w5XoErHzWLEPSpex9ljuASSoN0rRatVc2Ba74NnKWk7w6pWpttz8
-#    2njm1uVx/T5dbYa8iQgEAdt5jT5MXun4xcNTxGRec0yc84N7L9MZps0ry5F6
-#    YA4H9ibQ/4j+QdP8Y2Un4oLsNC2YCNXxuCdfb4fSvXcrU2sZZn8bB8NWD6Ab
-#    oaaPSU/TWrcm8s63OffGPWvcWvAxGx86Nv40yNhdbd8nFyQgd8IEGI9Cml8N
-#    hXSEiOs5HpuN0D59Tah1KzjoPBedAK2kh9mhoHJhF8C824xZBAC89iMgOChQ
-#    K8YQ3KnXiQDb0NJHOXcg9GcKFFULfHHmfvj5P/FDq9aoc7VVoQNmUZCRJfs9
-#    Xb+oaMr8kwI7IHZqDGcqDOvMw255c0isCZU4loXB9sFb/kyClFxVTHXZ8tcL
-#    ilt5Snshfyarrnj5CVKSVWVg5nibQJ1IR7AUOQyJrQQA6OnPfoMmYCZZ9lj1
-#    FPb8IzHGURrojL78pHb1Pf6l9t5PkAINCjR7qqSVRniRkIorHnLN2gwpAjxm
-#    /LcfwBGKt17QnVVHy5gKL+6itf+peZwB8bvxQFCXWNeUYbGIMlLnjRkM7kub
-#    zEV5Mj7O4SsAQAi/CQFxtyQrc3piZRcI7ckD+wdMtlF+sq9QEp1Amh+hjC3u
-#    Ld2Fo0sFJyXT4bWu5T9xH/P57yLVTu9eX2MBXA0UHonXCmY0MBiIXFPz1SKd
-#    ZzXqVPjyPuW/m6h3U5VtrvQgT2cQ+pVA8ie070dxvAAuggOmarjVoCTAozmS
-#    BtmtKKAmhdnwr2WqFldaWtZm//jKQb3NNXVoemdzbHZ0cGhAcHJvdG9ubWFp
-#    bC5jb20gPHVoemdzbHZ0cGhAcHJvdG9ubWFpbC5jb20+wsCNBBABCAAgBQJf
-#    qQBWBgsJBwgDAgQVCAoCBBYCAQACGQECGwMCHgEAIQkQyJjqI3ij62YWIQQt
-#    JDJZtpZ55QvHMjPImOojeKPrZk+RB/9pwanjhRLfd5l1gDHrbQaBhu+dsnA8
-#    J8iojWojO4+z50CSirICz+3ouM3jlKQwoWiKhk9bgPUS5UE1Vi+P6BoxZ9Pd
-#    X20437Fu/C7xpzGp4+RBYuKRWQy/JsCGgfo703hKosb67XuUCOrJc3+qeobE
-#    OTIYEMedfHg+dpEBMxkPBWtl3uurm1PYF7Zo4LA3MQTSmCh58F2vj4MX9CSm
-#    WE8EkFSJTNfTikAiZEyKhBfsul6OEtHpqjUVx5d3VTJ8zKP454Ah5HHN2HY2
-#    X6qGzmtGMHjncvMwWurlrMTOI+yoZXGowK80CaHj7S/x1b8Mm7TjhaHR6s5C
-#    Zbyaf2MrGNBdx8LYBF+pAFYBCACwhBpeD1fbOoA5v0ZxPsyRkONmQZKdUS+o
-#    lRnQlXQ3hqALHiKS0h4+tdcN33+T0l+OCXfc8ARXdEAD4ZRjgmrWZqDlMQ7A
-#    UhzYARLVMOMsnadlQg3oeeXNRnnNAIcCB5r9FQAz5CIvWCDKVCPMib9kEDOP
-#    a86pmDA0NIqWqRbiwjGR/RjFH8at9HAwSovfuf7jzoLxdvNosZwSMMghYNBX
-#    G9bKwCFhIKS1M8z62LsX6665N1W5IOrFvhZnf7T5kvnGyOKD1XShqRjTfFix
-#    JQ5gM3z7Rp9enPplW0rMfW9l3eA0VLfCuourR+38hox1UGjYHrFHmdtlkqq5
-#    xDF92GW/ABEBAAEAB/9WevpiH/lGzrQY1sYf4IkgPlCy/ihjS+3iFd62ayqr
-#    s5kAqWx961TnG3b39IqQNU8cizX++FbgM/PTEvd9A13tmg11DKJyBijciTYd
-#    XhzG9JyZRxICVoqIlmFBV2NN0sUT/nCBe+FlubMi/bMmWEx9LVTdjQ+M1qOz
-#    SBSoOMKeKHzk3lZTmAmMsUwaPJVFjhX0pVNBbQkS8qMLy8y+Wq5aApaHm9NS
-#    A0e8OoiYJyAXFtjWt/bcocnSqlK48A4ZeNBYdYgD0T1abea5eiUPyczKU6Kh
-#    RSOAREnwUPY/J/SGsCcmqrYY0hSabcUqv4flTY6k5AB2svxG2QzOUI7h8LLB
-#    BAC4FdY49I8hKBfpEMosv1RjYLD6cGC/YmewDTCGOOOxD8mieI5bAbTS7LSv
-#    shEVBmRqHiYwKPsUaC3SvXRS/ZsI7DLD1ZCh9IIA73T9PGglh0bJOms1lWmz
-#    u7yphdcbZv/eCmW5QPLlrNDh2nvhd0BxiQWp/X4vHKZNqHCCKzw0/wQA9XlF
-#    kpfUml7xT51v3Q3x0enyzYpH/pDimKmm53Mbe8yjN6XxkbRhW2FtiAt5VxxH
-#    yrLT9yzzcMtTgdjpTVBqNfowMI2oSdCohXC1c14SZy6XO3hOD/SNDdGHdMAA
-#    KoHRWkdKh5oTedXtb+0s4wMGolVhGey4nPE0wsaiG/+MD0ED/2pO+i5giLfu
-#    U2hvOfI+LzNEPir8VCoDIkHQAmYKWTQjtPUt+D5OE7a5JUDJGPNWkGfdJVc/
-#    Yfd2Z3k8RC1e6s6xAgmk8jfs6OySn1SWUuFzqIUNezec47klTagbhNZ/6PuI
-#    n+ggdZLXx3g5SbdpRI6sI9ssR6JVb7eBurWMxiwwQWXCwHYEGAEIAAkFAl+p
-#    AFYCGwwAIQkQyJjqI3ij62YWIQQtJDJZtpZ55QvHMjPImOojeKPrZkFfB/94
-#    dRmTiUBT1mGj9nLROMV8Ww0hjn97tZcfDbpMqROJRs0gYxEE13D0T6L2jAkh
-#    eSBgwErSOojI0B/Ft5O52D2iV3YCep/KsPnPewOGFAwFxnBKmYKxKUbZs6Nj
-#    dcQrd5/wBVQnUhlOmNk2q09zH5mQ+q1WU5slGEm0/ISYh1N03pB5aaFcPHee
-#    B+B/5KGkEAvVAPxIPUm8Ywhdkcsq5j3xrI96ZxkITjtABkWbqkkF0mqA9BL8
-#    ixlBVBUYMe5T9aRtldgvNd/XRYxgO20xrdcDGPJMFb3AWS0M31p8TAI0gfyN
-#    W4TxCAtK015bgKRgTmmBe5nE613hXjsUewaR4uaw
-#    =Niwe
-#    -----END PGP PRIVATE KEY BLOCK-----
-
 """ProtonMail private key encryption simulation
 
 Simulate the encryption/decryption of a private key in ProtonMail given a user
-password and a salt. The data to encrypt/decrypt is read from stdin, output is
-written to stdout. Input can be optionally specified as an armored PGP key.
+password and a salt. The data to encrypt/decrypt is read from stdin and the
+encrypted/decrypted data is written to stdout. The input can be raw data or an
+ASCII armored PGP private key (if the --armored option is set).
 
 Usage:
   pm.py encrypt [-a|--armored] <password> <salt>
@@ -146,15 +13,21 @@ Usage:
   pm.py (-h|--help)
 
 Arguments:
-  <password>  An arbitrary user password
-  <salt>      A random salt of 16 bytes (128 bits), Base64-encoded
+  <password>  The user password
+  <salt>      A Base64-encoded salt for hashing the password (16 bytes = 128 bits)
 
 Options:
-  -a --armored  Interpret input as armored PGP key (encryption) or write output
-                as armored PGP key (decryption)
+  -a --armored  Interpret input and write output as ASCII armored PGP key
   -h --help     Show this help message
+
+Dependencies:
+  - pgpy (https://pypi.org/project/PGPy/)
+  - bcrypt (https://pypi.org/project/bcrypt/)
+  - libscrc (https://pypi.org/project/libscrc/)
+  - cryptography (https://pypi.org/project/cryptography/)
+  - docopt (https://pypi.org/project/docopt/)
 """
-from docopt import docopt
+
 import os
 import sys
 import re
@@ -162,6 +35,7 @@ import base64
 import bcrypt
 import _bcrypt
 import libscrc
+from docopt import docopt
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives import padding
 from pgpy.packet.fields import String2Key
@@ -173,9 +47,8 @@ def main(args):
     password = bytes(args['<password>'], 'utf-8')
     salt = base64.b64decode(args['<salt>'])
 
-    # Derive AES256 encryption/decryption key
-    password_hash = hash_password(password, salt)
-    key = s2k(password_hash)
+    hash = password_hash(password, salt)
+    key = s2k(hash)
 
     if args['encrypt']:
         if args['--armored']:
@@ -192,13 +65,14 @@ def main(args):
             plaintext = armor(plaintext)
         sys.stdout.buffer.write(plaintext)
 
-# Convert a PGP private key into the armored ASCII format
-# OpenPGP ASCII armor format: https://tools.ietf.org/html/rfc4880#section-6
-# Args:
-#   data (bytes): the private key
-# Return:
-#   bytes: the private key in ASCII armored format
 def armor(data):
+    """Convert a PGP private key into ASCII armored format. The ASCII armored
+    format is described here: https://tools.ietf.org/html/rfc4880#section-6
+    Args:
+        data (bytes): the private key
+    Return:
+        bytes: the ASCII armored private key as a UTF-8 encoded byte array
+    """
     checksum = crc24(data)
     key = str(base64.b64encode(data), 'utf-8')
     key = re.sub("(.{60})", "\\1\n", key, 0, re.DOTALL).strip()
@@ -209,13 +83,14 @@ def armor(data):
 -----END PGP PRIVATE KEY BLOCK-----
 """, 'utf-8')
 
-# Extract the key data from an ASCII armored PGP key
-# OpenPGP ASCII armor format: https://tools.ietf.org/html/rfc4880#section-6
-# Args:
-#   data (bytes): an ASCII armored PGP key
-# Return:
-#   bytes: the extracted key data
 def unarmor(text):
+    """Extract the key data from an ASCII armored PGP key. The ASCII armored
+    format is described here: https://tools.ietf.org/html/rfc4880#section-6
+    Args:
+        text (bytes): an ASCII armored PGP key as a UTF-8 encoded byte array
+    Return:
+        bytes: the extracted key data
+    """
     text = str(text, 'utf-8')
     lines = text.splitlines()
     first, last = None, None
@@ -234,104 +109,90 @@ def unarmor(text):
     return data
 
 def crc24(data):
+    """Calculate CRC-24 checksum as described in https://tools.ietf.org/html/rfc4880#section-6.1
+    and encode it as four Base64 characters (24 bits).
+    Args:
+        data (bytes): the data for which to calculate the checksum
+    Return:
+        str: the checksum as a string of four Base64 characters
+    """
     return str(base64.b64encode(libscrc.openpgp(data).to_bytes(3, 'big')), 'utf-8')
 
-# Format a byte array as a string of hexadecimal digits 
-# Args:
-#   arr (bytes): a byte array
-# Return:
-#   (str): a string of space-separated hexadecimal digits (e.g. "E6 73 D2")
-def format(arr):
-    return ''.join(" {:02X}".format(e) for e in arr).strip()
-
-# Encrypt data
-# Args:
-#   data (bytes):    the data to encrypt
-#   password (str):  the password with which to encrypt the data
-#   salt (str)       a random salt for  hashing the password
-# Returns:
-#   bytes:  the encrypted data
-#def encrypt(data, password, salt, initvector):
-    # - If private key is armored, unarmor it (only the body, without delimiters, comments, and checksum) -> byte array
-    #     - Unarmor: extract body > delete newlines > Base64-decode
-    #     - Armor: Base64-encode > insert newline after each group of 60 characters
-    #              Get CRC-24 checksum of body > Base64-encode the checksum
-    #              Concatenate the following components: "-----BEGIN PGP PUBLIC KEY BLOCK-----", newline, Base64-encoded key, "=" and Base64-encoded checksum, "-----END PGP PUBLIC KEY BLOCK-----"
-    # - Derive an AES256 key from the passwd
-    # - Encrypt the byte array with AES256 and the derived key
-    # - Armor the encrypted key and return it
-
-# Encrypt data with AES256 and the provided key
-# Args:
-#   plaintext (bytes): the data to ecnrypt
-#   key (bytes):       an AES256 encryption key
-# Returns:
-#   bytes: the encrypted data
 def aes256_encrypt(plaintext, key):
+    """Encrypt data with AES-256 CBC mode and the provided key.
+    Args:
+        plaintext (bytes): the data to ecnrypt
+        key (bytes):       the AES-256 key for encryption (256 bits = 32 bytes)
+    Returns:
+        bytes: the encrypted data
+    """
     # Padding data to block size of AES CBC mode (128 bits = 16 bytes)
     padder = padding.PKCS7(128).padder()
     plaintext = padder.update(plaintext) + padder.finalize()
-
-    # Encrypt
+    # TODO: what initialisation vector to use?
     iv = os.urandom(16)
-    #iv = bytes("baaaaaaaaaaaaaaa", 'utf-8')
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
     encryptor = cipher.encryptor()
+    # TODO: how to include the initialisation vector in the ciphertext?
     return iv + encryptor.update(plaintext) + encryptor.finalize()
 
-# Decrypt data with AES256 and the provided key
-# Args:
-#   ciphertext (bytes): the ciphertext to decrypt
-#   key (bytes):        the AES256 key that was used for encrypting the data
-# Returns:
-#   bytes: the decrypted data
 def aes256_decrypt(ciphertext, key):
-    # Decrypt
+    """Decrypt ciphertext with AES-256 CBC mode and the provided key.
+    Args:
+        ciphertext (bytes): the ciphertext to decrypt
+        key (bytes):        the AES-256 key for decryption (256 bits = 32 bytes)
+    Returns:
+        bytes: the decrypted data
+    """
+    # TODO: how to handle the initialisation vector?
     iv, ciphertext = ciphertext[:16], ciphertext[16:]
     cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
     decryptor = cipher.decryptor()
     plaintext = decryptor.update(ciphertext) + decryptor.finalize()
-
     # Remove padding
     unpadder = padding.PKCS7(128).unpadder()
     return unpadder.update(plaintext) + unpadder.finalize()
 
-
-# Generate an AES256 key from a string
-# Args:
-#   secret (bytes): the secret to derive the key from
-# Returns:
-#   bytes: a valid AES256 key (256 bits = 32 bytes)
 def s2k(passphrase):
+    """Generate an AES-256 key from a passphrase ("string to key").
+    Args:
+        passphrase (bytes): the passphrase as a UTF-8 encoded byte array
+    Returns:
+        bytes: the derived AES-256 key (256 bits = 32 bytes)
+    """
     s2k = String2Key()
-    s2k.salt = b'aaaaaaaa'                 # TODO: what salt to use?
-    # s2k.usage = String2KeyType.Iterated  # TODO: not sure if needed
+    # TODO: what salt to use (must be static or must be passed in)?
+    s2k.salt = b'aaaaaaaa'
+    # TODO: what S2K type and hash algorithm to use?
     s2k.specifier = String2KeyType.Iterated
     s2k.encalg = SymmetricKeyAlgorithm.AES256
     s2k.halg = HashAlgorithm.SHA256
     return s2k.derive_key(str(passphrase, 'utf-8'))
 
-# Calculate the hash of a password
-# Args:
-#   password (bytes): a password of arbitrary length
-#   salt (bytes):     an arbitrary salt of 128 bits (16 bytes)
-# Returns:
-#   str: the hash of the password
-def hash_password(password, salt):
-    # Transform salt to bcrypt's Base64 format
-    #salt = base64.b64decode(salt)
-    # Encode salt in bcrypt's Base64 format
+def password_hash(password, salt):
+    """Hash the user password with the provided salt.
+    Args:
+        password (bytes): the user password as a UTF-8 encoded byte array
+        salt (bytes):     an arbitrary salt of 128 bits (16 bytes)
+    Returns:
+        bytes: the hashed password as a UTF-8 encoded byte array
+    """
+    # Encode salt in bcrypt's Base64 format and prepend bcrypt prefix
     salt_base64 = _bcrypt.ffi.new("char[]", 30)
     _bcrypt.lib.encode_base64(salt_base64, salt, 16)
-
-    # Prepend bcrypt prefix to Base64-encoded salt
     salt_base64 = bytes("$2y$10$", 'utf-8') + _bcrypt.ffi.string(salt_base64)
-
-    # Compute the has of the password
+    # Hash password and return everything after prefix and salt (first 29 chars)
     password_hash = bcrypt.hashpw(password, salt_base64)
-
-    # Cut off the first 29 bytes (prefix and salt) and return the rest
     return password_hash[29:]
+
+def format(arr):
+    """Format a byte array as a string of hexadecimal digits (for debugging).
+    Args:
+        arr (bytes): a byte array
+    Return:
+        str: a string of space-separated hexadecimal digits (e.g. "E6 73 D2")
+    """
+    return ''.join(" {:02X}".format(e) for e in arr).strip()
 
 if __name__ == '__main__':
     args = docopt(__doc__)
